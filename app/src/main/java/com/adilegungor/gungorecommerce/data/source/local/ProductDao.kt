92e4bfb3b93c5085b66dfc3453ec4a38ae1c6e3d
Adl1coder <adilegunngorr@gmail.com>
2023-09-30 17:03:26 +0300

    updated

--- app/src/main/java/com/adilegungor/gungorecommerce/data/source/local/ProductDao.kt
+++ app/src/main/java/com/adilegungor/gungorecommerce/data/source/local/ProductDao.kt
@@ -8,13 +8,16 @@ import androidx.room.Query
 import com.adilegungor.gungorecommerce.data.model.ProductEntity
 
 
-@Dao
+@Dao //sınıf:roomdao
 interface ProductDao {
 
-    @Query("SELECT * FROM cart_products")
+    @Query("SELECT * FROM cart_products")//get all pro
     suspend fun getProducts(): List<ProductEntity>
+    //product entity veri listesi
+     //askıya alınır işlem- tüm ürünleri al.
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
+    //onflict:çakışma=>yeniyi ekle
     suspend fun addProduct(product: ProductEntity)
 
     @Delete
