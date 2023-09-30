54e099c99cbbb4cffaff9e60a96eb1f69a705af5
Adl1coder <adilegunngorr@gmail.com>
2023-09-30 17:09:31 +0300

    updated

--- app/src/main/java/com/adilegungor/gungorecommerce/data/source/local/ProductRoomDB.kt
+++ app/src/main/java/com/adilegungor/gungorecommerce/data/source/local/ProductRoomDB.kt
@@ -6,8 +6,15 @@ import androidx.room.RoomDatabase
 import com.adilegungor.gungorecommerce.data.model.ProductEntity
 
 //favoriler için @Database(entities = [ProductEntity::class, ProductEntity::class], version = 1)
+//veritabanısınıf:local dbleri yönet.
+//[ProductEntity::class] hangi db kull.  tanım
 @Database(entities = [ProductEntity::class], version = 1)
+//db değiş. sürüm upd. et. vers.
+
+//roomdb local db işl. için.
+//kalıtım
 abstract class ProductRoomDB : RoomDatabase(){
 
     abstract fun productsDao(): ProductDao
+    //db işlemlerinin nin olduğu daoya erişim
 }
